e19a12d51454809b05a57e89ac7dab7b5f0a07e2
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-03-19 13:26:40 +0100

    ✨ feat: GDrum component init

---
+++ src/components/GDrum/GDrum.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { PropType } from "vue";
+
+// types
+type KbdKeyType = "A" | "S" | "D" | "F" | "G" | "H" | "J" | "K" | "L";
+type SoundType =
+  | "clap"
+  | "hihat"
+  | "kick"
+  | "openhat"
+  | "boom"
+  | "ride"
+  | "snare"
+  | "tom"
+  | "tink";
+
+const props = defineProps({
+  /**
+   * Keyboard key type ("A" | "S" | "D" | "F" | "G" | "H" | "J" | "K" | "L")
+   */
+  kbdKey: {
+    type: String as PropType<KbdKeyType>,
+    required: true,
+  },
+  /**
+   * Drum sound
+   */
+  sound: {
+    type: String as PropType<SoundType>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="drum">
+    <kbd class="kbd-key">{{ props.kbdKey }}</kbd>
+    <span class="sound">{{ props.sound }}</span>
+  </div>
+</template>
+
+<style lang="css" src="./GDrum.css"></style>
