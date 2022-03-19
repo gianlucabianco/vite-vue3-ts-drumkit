25f0af12c76e2639240767a79c85e3faa870feb0
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-03-19 14:46:27 +0100

    🎨 chore: GDrum is now in HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import { ref } from "vue";
+import GDrum from "@/components/GDrum/GDrum.vue";
+const isPlaying = ref(false);
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <GDrum
+      kbdKey="A"
+      sound="clap"
+      :is-playing="isPlaying"
+      @click="isPlaying = !isPlaying"
+    />
   </main>
 </template>
