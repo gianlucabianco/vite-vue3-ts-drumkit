1e0635759779d950e5557123307fe6c62cdead57
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-03-20 17:24:06 +0100

    ♻️ feat: HomeView component contains now GDrumKit component only

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import GDrum from "@/components/GDrum/GDrum.vue";
-const isPlaying = ref(false);
+import GDrumKit from "@/components/GDrumKit/GDrumKit.vue";
 </script>
 
 <template>
   <main>
-    <GDrum
-      kbdKey="A"
-      sound="clap"
-      :is-playing="isPlaying"
-      @click="isPlaying = !isPlaying"
-    />
+    <GDrumKit />
   </main>
 </template>
