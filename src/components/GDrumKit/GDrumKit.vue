7cfc057c6ca3fbdd8845db451f00b402d8b64554
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-03-20 17:22:39 +0100

    ✨ feat: GDrumKit wrapper component init

---
+++ src/components/GDrumKit/GDrumKit.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import keyCombos from "@/config/KeyCombos";
+import GDrum from "@/components/GDrum/GDrum.vue";
+</script>
+
+<template>
+  <div class="drum-kit">
+    <GDrum
+      v-for="(keyCombo, index) in keyCombos"
+      :key="`${index}-${keyCombo.drum}`"
+      :kbd-key="keyCombo.kbdKey"
+      :sound="keyCombo.drum"
+    />
+  </div>
+</template>
