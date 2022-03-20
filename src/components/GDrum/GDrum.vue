11c824aef7157729de85aba4148fa4e38140c65c
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-03-20 17:32:40 +0100

    ✨ feat: GDrum is now responsible of playing sounds and change its representational state

--- src/components/GDrum/GDrum.vue
+++ src/components/GDrum/GDrum.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
+import { ref, computed, onMounted } from "vue";
+
 import type { PropType } from "vue";
-import { computed } from "vue";
+import type { KbdKey, Sound, CssClasses, KeyCombo } from "@/config/types";
 
-import type { KbdKey, Sound, CssClasses } from "@/config/types";
+import userKeydown from "@/composables/UserKeydown";
+import { playSound } from "@/composables/PlaySound";
 
-// props
 const props = defineProps({
   /**
-   * Keyboard key type ("A" | "S" | "D" | "F" | "G" | "H" | "J" | "K" | "L")
+   * Keyboard key type.
+   * It could be configured in '@/config/KeyCombos'.
+   * To see the current configured kbdKeys, mouse hover the KbdKey type.
    */
   kbdKey: {
     type: String as PropType<KbdKey>,
@@ -15,27 +19,55 @@ const props = defineProps({
   },
   /**
    * Drum sound
+   * It could be configured in '@/config/KeyCombos'.
+   * To see the current configured kbdKeys, mouse hover the Sound type.
    */
   sound: {
     type: String as PropType<Sound>,
     required: true,
   },
-  /**
-   * Handles the representional state of the component
-   */
-  isPlaying: {
-    type: Boolean as PropType<boolean>,
-    default: false,
-  },
 });
 
-// computed
-const classes = computed((): CssClasses => {
-  const { isPlaying } = props;
+/**
+ * isPlaying stores the state internal state of the component.
+ * If true, the component is playing.
+ */
+const isPlaying = ref(false);
 
+/**
+ * togglePlay method toggles the state of isPlaying.
+ */
+const togglePlay = () => {
+  isPlaying.value = !isPlaying.value;
+};
+
+/**
+ * keyCombo is the key combination of keyboard key, sound and callback function
+ * that triggers the component.
+ */
+const keyCombo = {
+  kbdKey: props.kbdKey,
+  drum: props.sound,
+  action: () => playSound(props.sound, togglePlay),
+};
+
+/**
+ * cssClasses sets css dynamic classes for the component.
+ * It is computed from the props, and handles the representional
+ * state of the component.
+ */
+const classes = computed((): CssClasses => {
   return {
-    "is-playing": isPlaying,
+    "is-playing": isPlaying.value,
   };
+});
+
+onMounted(() => {
+  /**
+   * Register the keyCombo to the userKeydown.
+   * This will trigger the callback function when the keyCombo is pressed.
+   */
+  userKeydown([keyCombo as KeyCombo]);
 });
 </script>
 
