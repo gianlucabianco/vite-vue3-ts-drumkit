7986530aa1a747c7e9a71a454282912af87b6f90
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-03-20 19:56:39 +0100

    🚚 chore: HomeView component has now its own folder

---
+++ src/views/HomeView/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import GDrumKit from "@/components/GDrumKit/GDrumKit.vue";
+</script>
+
+<template>
+  <main class="home-view">
+    <GDrumKit />
+  </main>
+</template>
+
+<style lang="css" src="./HomeView.css"></style>
