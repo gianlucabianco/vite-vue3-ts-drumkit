a3c6a1b86de19840ca29cba1b72239dc5b012945
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-03-20 19:36:11 +0100

    🎨 feat: KeyCombos has now kbdKeyNames

--- src/config/KeyCombos.ts
+++ src/config/KeyCombos.ts
@@ -9,38 +9,47 @@ import type { KeyCombos } from "@/config/types";
 const keyCombos: KeyCombos = [
   {
     kbdKey: "KeyA",
+    kbdKeyName: "A",
     drum: "clap",
   },
   {
     kbdKey: "KeyS",
+    kbdKeyName: "S",
     drum: "hihat",
   },
   {
     kbdKey: "KeyD",
+    kbdKeyName: "D",
     drum: "kick",
   },
   {
     kbdKey: "KeyF",
+    kbdKeyName: "F",
     drum: "openhat",
   },
   {
     kbdKey: "KeyG",
+    kbdKeyName: "G",
     drum: "boom",
   },
   {
     kbdKey: "KeyH",
+    kbdKeyName: "H",
     drum: "ride",
   },
   {
     kbdKey: "KeyJ",
+    kbdKeyName: "J",
     drum: "snare",
   },
   {
     kbdKey: "KeyK",
+    kbdKeyName: "K",
     drum: "tom",
   },
   {
     kbdKey: "KeyL",
+    kbdKeyName: "L",
     drum: "tink",
   },
 ];
